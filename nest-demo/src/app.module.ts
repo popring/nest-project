24cb1cebd74218c08ebadcae76ed88079d89a203
Harry Hao <koler778@gmail.com>
2025-01-24 17:49:10 +0800

    feat: add OtherModule and integrate OtherService into AppService

--- nest-demo/src/app.module.ts
+++ nest-demo/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OtherModule } from './other/other.module';
 
 @Module({
-  imports: [],
+  imports: [OtherModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ nest-demo/src/other/other.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class OtherService {
+  getOther(): string {
+    return 'other';
+  }
+}
